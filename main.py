ba26fc3645599d9563f4bee01a3c796a0d1e31a9
Priyesh Ranjan <55106528+Priyesh-Ranjan@users.noreply.github.com>
2024-11-15 12:17:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import parser
+import variables
 import process
 
 if __name__ == "__main__":
-    args = parser.parse_args()
+    args = variables.parse_args()
     print("#" * 64)
     for i in vars(args):
         print(f"#{i:>40}: {str(getattr(args, i)):<20}#")
