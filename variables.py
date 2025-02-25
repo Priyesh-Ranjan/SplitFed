0809b102b090d90d46890d188c2d06825100bc30
Priyesh Ranjan <55106528+Priyesh-Ranjan@users.noreply.github.com>
2025-02-25 11:12:36 -0600

    Update variables.py

--- variables.py
+++ variables.py
@@ -7,7 +7,7 @@ def parse_args():
     parser.add_argument("--test_batch_size", type=int, default=64)
     parser.add_argument("--epochs", type=int, default=10)
     parser.add_argument("--optimizer", type=str, default='SGD')
-    parser.add_argument("--lr", type=float, default=0.01, help="Learning rate of models")
+    parser.add_argument("--lr", type=float, default=0.1, help="Learning rate of models")
     parser.add_argument("--momentum", type=float, default=0.5)
     parser.add_argument("--seed", type=int, default=1)
     parser.add_argument("--num_clients", type=int, default=10)
